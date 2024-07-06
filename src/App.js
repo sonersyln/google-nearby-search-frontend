6fe8744cf54b6e157627e2c6beee23c7e159c224
Soner <syln.soner@gmail.com>
2024-07-06 13:20:02 +0300

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
