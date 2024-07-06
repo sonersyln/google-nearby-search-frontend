839b3fe64a2b203a1587bd6afcf230d7e677d384
Soner <syln.soner@gmail.com>
2024-07-06 13:39:51 +0300

    Refactor Map component and related files

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,51 +1,61 @@
-import React, { useState } from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
-import googleMapsApiKey from '../../config/config';
-import './Map.css';
+import React, { useState } from "react";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import googleMapsApiKey from "../../config/config";
+import "./Map.css";
 
 const mapContainerStyle = {
-    width: '80%',
-    height: '700px',
-    marginTop: '20px'
+  width: "80%",
+  height: "700px",
+  marginTop: "20px",
 };
 
 const Map = ({ center, places }) => {
-    const [selectedPlace, setSelectedPlace] = useState(null);
+  const [selectedPlace, setSelectedPlace] = useState(null);
 
-    return (
-        <LoadScript
-            googleMapsApiKey={googleMapsApiKey}
-            loadingElement={<div>Loading...</div>}
-            libraries={['places']}
+  return (
+    <div className="map-container">
+      <LoadScript
+        googleMapsApiKey={googleMapsApiKey}
+        loadingElement={<div>Loading...</div>}
+        libraries={["places"]}
+      >
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={14}
         >
-            <GoogleMap
-                mapContainerStyle={mapContainerStyle}
-                center={center}
-                zoom={14}
+          {places.map((place, index) => (
+            <Marker
+              key={index}
+              position={{ lat: place.latitude, lng: place.longitude }}
+              title={place.name}
+              onClick={() => setSelectedPlace(place)}
+            />
+          ))}
+          {selectedPlace && (
+            <InfoWindow
+              position={{
+                lat: selectedPlace.latitude,
+                lng: selectedPlace.longitude,
+              }}
+              onCloseClick={() => setSelectedPlace(null)}
             >
-                {places.map((place, index) => (
-                    <Marker
-                        key={index}
-                        position={{ lat: place.latitude, lng: place.longitude }}
-                        title={place.name}
-                        onClick={() => setSelectedPlace(place)}
-                    />
-                ))}
-                {selectedPlace && (
-                    <InfoWindow
-                        position={{ lat: selectedPlace.latitude, lng: selectedPlace.longitude }}
-                        onCloseClick={() => setSelectedPlace(null)}
-                    >
-                        <div>
-                            <h2>{selectedPlace.name}</h2>
-                            <p>Description: {selectedPlace.description}</p>
-                            <p>Address: {selectedPlace.address}</p>
-                        </div>
-                    </InfoWindow>
-                )}
-            </GoogleMap>
-        </LoadScript>
-    );
+              <div>
+                <h2>{selectedPlace.name}</h2>
+                <p>Description: {selectedPlace.description}</p>
+                <p>Address: {selectedPlace.address}</p>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
 };
 
 export default Map;
